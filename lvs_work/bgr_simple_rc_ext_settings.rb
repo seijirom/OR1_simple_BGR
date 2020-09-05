079267e8d69d2c23637141bd7dde371fab785c25
seijirom <seijiro.moriyama@gmail.com>
2020-09-05 17:09:31 +0900

    DRC/LVS clean on KLayout

--- lvs_work/bgr_simple_rc_ext_settings.rb
+++ lvs_work/bgr_simple_rc_ext_settings.rb
@@ -5,6 +5,9 @@ def rc_ext_settings
   same_device_classes 'NMOS', 'NCH'
   same_device_classes 'HRES', 'RES'
   same_device_classes 'RES', 'RES'
+  tolerance 'HRES', 'R', relative: 0.03
+  tolerance 'RES', 'R', relative: 0.03
+  tolerance 'CAP', 'C', relative: 0.03
   netlist.flatten_circuit 'Nch*'
   netlist.flatten_circuit 'Pch*'
   netlist.flatten_circuit 'R_poly*'
