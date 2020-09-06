c2b90217542439dae13b814a9959bf8317c8d11a
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2020-09-07 00:17:29 +0900

    replaced nch20x1 and pch20x1 with OR1 pcells, resolved off-grid issue

--- lvs_work/bgr_simple_rc_ext_settings.rb
+++ lvs_work/bgr_simple_rc_ext_settings.rb
@@ -5,9 +5,10 @@ def rc_ext_settings
   same_device_classes 'NMOS', 'NCH'
   same_device_classes 'HRES', 'RES'
   same_device_classes 'RES', 'RES'
-  tolerance 'HRES', 'R', relative: 0.03
-  tolerance 'RES', 'R', relative: 0.03
-  tolerance 'CAP', 'C', relative: 0.03
+  same_device_classes 'D', 'D'
+  #tolerance 'HRES', 'R', relative: 0.03
+  #tolerance 'RES', 'R', relative: 0.03
+  #tolerance 'CAP', 'C', relative: 0.03
   netlist.flatten_circuit 'Nch*'
   netlist.flatten_circuit 'Pch*'
   netlist.flatten_circuit 'R_poly*'
