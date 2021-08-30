51937af53e12a08027805be983cec83a050731d0
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2021-08-30 23:43:08 +0900

    DRC and LVS clean for 2021_9to; 1Meg ohm resistor disconnected from Vin and assigned Rex pin

--- bgr_simple.asy
+++ bgr_simple.asy
@@ -1,10 +1,13 @@
 Version 4
 SymbolType BLOCK
-RECTANGLE Normal -64 -24 80 24
-WINDOW 0 8 -24 Bottom 2
+RECTANGLE Normal -64 -40 80 24
+WINDOW 0 8 -33 Bottom 2
 PIN -64 0 LEFT 8
 PINATTR PinName vin
 PINATTR SpiceOrder 1
 PIN 80 0 RIGHT 8
 PINATTR PinName vout
 PINATTR SpiceOrder 2
+PIN -64 -32 LEFT 8
+PINATTR PinName Rex
+PINATTR SpiceOrder 3
--- lvs_work/bgr_simple_rc_ext_settings.rb
+++ lvs_work/bgr_simple_rc_ext_settings.rb
@@ -1,14 +1,11 @@
 def rc_ext_settings
   same_circuits 'bgr_simple', 'BGR_SIMPLE'
   align
-  same_device_classes 'PMOS', 'PCH'
-  same_device_classes 'NMOS', 'NCH'
   same_device_classes 'HRES', 'RES'
   same_device_classes 'RES', 'RES'
-  same_device_classes 'D', 'D'
-  #tolerance 'HRES', 'R', relative: 0.03
-  #tolerance 'RES', 'R', relative: 0.03
-  #tolerance 'CAP', 'C', relative: 0.03
+  tolerance 'HRES', 'R', relative: 0.1
+  tolerance 'RES', 'R', relative: 0.03
+  tolerance 'CAP', 'C', relative: 0.03
   netlist.flatten_circuit 'Nch*'
   netlist.flatten_circuit 'Pch*'
   netlist.flatten_circuit 'R_poly*'
